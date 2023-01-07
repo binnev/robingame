d8b711109920b9dc79110c96b458a1d1c94db214
binnev <byntje@gmail.com>
2023-01-07 22:34:38 +0100

    corrected typing on ease functions

--- robingame/animation.py
+++ robingame/animation.py
@@ -18,17 +18,17 @@ def ease(
     return value
 
 
-def ease_in(x, start, stop, num, power=3) -> numpy.array:
+def ease_in(x, start, stop, num, power=3) -> float:
     f = lambda x: x**power
     return ease(x=x, start=start, stop=stop, num=num, function=f)
 
 
-def ease_out(x, start, stop, num, power=3) -> numpy.array:
+def ease_out(x, start, stop, num, power=3) -> float:
     f = lambda x: 1 - (1 - x) ** power
     return ease(x=x, start=start, stop=stop, num=num, function=f)
 
 
-def ease_in_out(x, start, stop, num, power=3) -> numpy.array:
+def ease_in_out(x, start, stop, num, power=3) -> float:
     f = lambda x: 4 * x**power if x < 0.5 else 1 - (-2 * x + 2) ** power / 2
     return ease(x=x, start=start, stop=stop, num=num, function=f)
 
