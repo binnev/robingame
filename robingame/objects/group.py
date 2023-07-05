452ed9c3d87c0213a9334a0a10adc7e7b03334c9
binnev <byntje@gmail.com>
2023-07-05 11:06:48 +0200

    cleaned up Group

--- robingame/objects/group.py
+++ robingame/objects/group.py
@@ -8,14 +8,20 @@ class Group(pygame.sprite.Group):
         super().update(*args)
 
     def draw(self, surface: pygame.Surface, debug: bool = False):
-        """Draws all of the member sprites onto the given surface."""
-        sprites = self.sprites()
-        for sprite in sprites:
-            sprite.draw(surface, debug)
+        """
+        Draws all of the member sprites onto the given surface.
+        Different from pygame's Group.draw in that it calls the Entity.draw() method instead of
+        just blitting the Entity.image to the screen.
+        """
+        entities = self.sprites()
+        for entity in entities:
+            entity.draw(surface, debug)
         self.lostsprites = []
 
     def kill(self):
-        """Kill all the sprites in this group. This is different from .empty().
-        empty() does not kill the sprites in other groups."""
+        """
+        Kill all the sprites in this group. This is different from .empty().
+        Does not kill the sprites in other groups.
+        """
         for sprite in self:
             sprite.kill()
