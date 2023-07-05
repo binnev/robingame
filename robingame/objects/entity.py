6a3e9e55201883d78e5be63ac5cc0bc922eacbe7
binnev <byntje@gmail.com>
2023-07-05 10:40:32 +0200

    Cleaned up Entity

--- robingame/objects/entity.py
+++ robingame/objects/entity.py
@@ -1,9 +1,8 @@
+from typing import Callable, Iterable
+
 import pygame
-from pygame import Color
 from pygame import Surface
 from pygame.rect import Rect
-from pygame.sprite import AbstractGroup
-from typing import Callable
 
 from robingame.objects.group import Group
 
@@ -11,33 +10,49 @@ from robingame.objects.group import Group
 class Entity(pygame.sprite.Sprite):
     """
     Finite State Machine:
-    - self.tick is incremented every time the main game loop executes
     - self.state is executed every tick
-    - when self.state changes, self.tick is set to 0
+    - self.tick is incremented every time the main game loop executes
+    - when self.state changes, self.tick is set to 0, so you can track how long the entity has
+      been in its current state
 
     Hierarchical structure:
     - Entities can be added to Groups to create a hierarchical structure
-    - The order of groups in the .groups attribute determines the draw order; it's basically the
-    layers
+    - The order of groups in the .child_groups attribute determines the draw order; it's basically
+      the layers
+
+    Example game structure:
+    Game: Entity
+    └── scenes: Group
+        └── MainMenu: Entity
+            ├── buttons: Group
+            │   ├── Button: Entity
+            │   ├── Button: Entity
+            │   └── Button: Entity
+            └── sliders: Group
+                ├── Slider: Entity
+                └── Slider: Entity
+
     """
 
     _state: Callable = lambda *args, **kwargs: None
-    child_groups: list  # groups of child Entities belonging to this entity
+    child_groups: list[Group]  # groups of child Entities belonging to this entity
     tick: int = 0  # iterations of the main game loop
-    parental_name = "parent"
-    debug_color = Color("red")
 
-    def __init__(self, *groups: AbstractGroup) -> None:
+    def __init__(self, groups: Iterable[Group] = ()) -> None:
         super().__init__(*groups)
         self.child_groups = []
 
     def update(self):
-        self.state()
+        self.state()  # execute current state function
+        # update all child groups. This allows the entire tree of objects to update by only
+        # calling the .update() method of the root object
         for group in self.child_groups:
             group.update()
-        self.tick += 1
+        self.tick += 1  # increment tick to keep track of how long we've been in the current state
 
     def draw(self, surface: Surface, debug: bool = False):
+        # draw all child groups. This allows the entire tree of objects to draw by only calling
+        # the .draw() method of the root object.
         for group in self.child_groups:
             group.draw(surface, debug)
 
@@ -47,26 +62,15 @@ class Entity(pygame.sprite.Sprite):
             group.kill()
         super().kill()
 
-    def add_to_group(self, *objects, group: Group):
-        """
-        Add an object to one of self.child_groups and give the object a reference to self as parent.
-        This method is intended to be used by more specific methods e.g.:
-        def add_particle(*objects):
-            self.add_to_group(*objects, self.particles)
-        """
-        group.add(*objects)
-        # give the object a reference to this scene
-        for obj in objects:
-            setattr(obj, self.parental_name, self)
-
     @property
     def state(self):
         return self._state
 
     @state.setter
     def state(self, new_state):
-        """Reset self.tick when state changes so we know how long we've been in the current
-        state."""
+        """
+        Reset self.tick when state changes so we know how long we've been in the current state.
+        """
         self._state = new_state
         self.tick = 0
 
