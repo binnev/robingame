8fa040f4dd1d9665c3267386fb88d8341331e549
binnev <byntje@gmail.com>
2023-09-01 09:00:51 +0200

    fix(text): Added a better test text to font_sandbox.py.

--- robingame/examples/font_sandbox.py
+++ robingame/examples/font_sandbox.py
@@ -5,32 +5,33 @@ from pygame.surface import Surface
 from robingame.objects import Game
 from robingame.text import fonts
 
-snippet = """
- 1space
-  2space
-Ook    in 
-de     spelling 
-van    sommige andere talen wordt de umlaut gebruikt, bijvoorbeeld in verwante 
-talen als het IJslands en het Zweeds, om een soortgelijk 
+snippet = r"""THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG 
+the quick brown fox jumps over the lazy dog 
+Numbers 1234567890
+Punctuation `~!@#$%^&*()-=_+[]\{}|;':",./<>?
+Accents ÀÈÌÒÙ àèìòù ÁÉÍÓÚÝ áéíóúý ÂÊÎÔÛ âêîôû ÃÑÕ ãñõ ÄËÏÖÜŸ äëïöüÿ
 
-klankverschijnsel
-weer
-te;;;'''
+Mötörhêàd
 
-geven, of in niet-verwante talen als het Fins, het Hongaars of het Turks, om dezelfde e-, eu- en u-klank weer te geven, die echter niet het resultaat van hetzelfde klankverschijnsel zijn.
+code snippet with indentation
+for ii in range(3): 
+    for jj in range(9): 
+        print(math.sqrt(ii**2 + jj**2))
 """
 
 
 class FontTest(Game):
+    font = fonts.cellphone_white  # the font to test
     window_width = 1500
+    window_height = 1000
     screen_color = (150, 150, 150)
 
     def draw(self, surface: Surface, debug: bool = False):
         super().draw(surface, debug)
-        X = 500
+        X = 50
         Y = 30
-        WRAP = 500
-        fonts.cellphone_white.render(
+        WRAP = 1400
+        self.font.render(
             surface,
             snippet,
             scale=2,
